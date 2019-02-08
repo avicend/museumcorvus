f97a3dd21eb43d698f447ea924d18dff7d5604da
Utku Mert Değirmenci <utku.d.m@gmail.com>
2019-02-08 15:28:47 +0200

    Add files via upload

---
+++ InteractScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractScript : MonoBehaviour
+{
+    public float interactDistance = 5f;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray,out hit, interactDistance))
+            {
+                if (hit.collider.CompareTag("Door"))
+                {
+                    hit.collider.transform.GetComponent<DoorScript>().ChangeDoorState();
+                }
+            }
+        }
+    }
+}
---
+++ MicrowaveOpen.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+public class MicrowaveOpen : MonoBehaviour
+{
+    public bool erko = false;
+    public bool kari = false;
+    public bool kara = false;
+    public bool nine = false;
+
+    public bool flag = false;
+
+    public AudioSource bellSoundSource;
+
+    /*public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;*/
+
+    public FirstPersonController fpsScript;
+
+    public GameObject door;
+    private DoorScript doorScript;
+    
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+        doorScript = door.GetComponent<DoorScript>();
+    }
+    void Update()
+    {
+        if (!flag)
+        {
+
+            if (erko && kari && kara && nine)
+            {
+                door.GetComponent<BoxCollider>().enabled = false;
+                doorScript.ChangeDoorState();
+                
+                bellSoundSource.Play();
+                flag = true;
+            }
+        }
+
+         
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+
+            if (other.gameObject.tag == "ErkebitStatue")
+            {
+                erko = true;
+            }
+            if (other.gameObject.tag == "AlkarısıStatue")
+            {
+                kari = true;
+            }
+            if (other.gameObject.tag == "KarakoncolosStatue")
+            {
+                kara = true;
+            }
+            if (other.gameObject.tag == "ÇayninesiStatue")
+            {
+                nine = true;
+            }
+
+
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="SlowArea")
+        {
+            
+                fpsScript.amISlowed = true;
+            
+        }
+    }
+
+
+    
+
+
+
+}
